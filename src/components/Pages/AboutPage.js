a79d7f19f93ee0c843f38c3ba881275d381232d1
sorano <rokudo.ananas.mukuro@gmail.com>
2021-08-31 15:16:48 +0300

    about

--- src/components/Pages/AboutPage.js
+++ src/components/Pages/AboutPage.js
@@ -1,10 +1,29 @@
 import React from 'react'
-
-export const AboutPage = () =>(
+import { NavLink } from 'react-router-dom'
+export const AboutPage = () => (
    <div className='AboutPage'>
-      <div className='AboutPage__content'>
-         <div> 
-            Практическое копирование сайта(фронт-енд части) по разным тестам
+      <div className='AboutPage__content m-5'>
+         <div class="p-5 mb-4 bg-light rounded-3">
+            <div class="container-fluid py-5">
+               <h1 class="display-5 fw-bold">Приветствую вас</h1>
+               <p class="col-md-8 fs-4">Кратко расскажу о том чем я тут занимаюсь. Работаю для портфолио и для души.Пока ищу работу в сфере разработки паралельно изучаю новые фичи и библиотеки.</p>
+               <NavLink class="btn btn-dark btn-lg" to='/'>Обратно на главную</NavLink>
+            </div>
+         </div>
+
+         <div class="row align-items-md-stretch mb-4">
+            <div class="col-md-6">
+               <div class="h-100 p-5 text-white bg-dark rounded-3">
+                  <h2>Знания на данный момент</h2>
+                  <p>На данный момент разрабатываю для практики и умения. Не хочу ограничиваться знаниями по front-end разработке. В данный момент не хватает практики в Redux</p>
+               </div>
+            </div>
+            <div class="col-md-6">
+               <div class="h-100 p-5 bg-light border rounded-3">
+                  <h2>То к чему я стремлюсь</h2>
+                  <p>Знать все не возможно и не нужно.Но я хочу овладеть в ближайшее время full stack разработкой и такими вещами как TypeScript и Node + библиотеки и в дальнейшем я думаю попробовать себе еще в других фреймворках</p>
+               </div>
+            </div>
          </div>
       </div>
    </div>
--- src/components/Pages/PopularPage.js
+++ src/components/Pages/PopularPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { Card } from '../Parts/Card'
 import {asyncPop} from '..//..//Redux/action'
